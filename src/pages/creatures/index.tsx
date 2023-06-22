74514cb0bf3c38c6b50aa4ff7c76d4c8e479ce5c
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-06-22 17:02:24 -0300

    Adding creatures page

---
+++ src/pages/creatures/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Creatures() {
+    return (
+        <>
+          <h1>Creatures</h1>
+          <h2>
+            <Link href="/">Back to home</Link>
+          </h2>
+        </>
+      );
+}
