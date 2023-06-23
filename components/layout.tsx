8f60b32651c30336f596d5827ea3dd0189b5c96d
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-06-23 10:59:43 -0300

    Finishing Layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,54 @@
 import { ReactNode } from "react";
-import styles from './layout.module.css';
+import Head from 'next/head';
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Layout({ children }: { children: ReactNode }) {
+import styles from './layout.module.css';
+import utilStyles from '@/styles/utils.module.css';
+
+export const siteTitle = 'Reviewing Next.js';
+
+export default function Layout({ children, home }: { children: ReactNode, home?: boolean }) {
   return (
     <div className={styles.container}>
-      {children}
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Reviewing Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {
+          home ? (
+            <>
+              <Image
+                priority
+                src="/images/Gaz'Haragoth.gif"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt=""
+              />
+            </>
+          ) : (
+            <>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/" className={utilStyles.colorInherit}>
+                  Home
+                </Link>
+              </h2>
+            </>
+          )
+        }
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 import Head from 'next/head';
+import Layout from '@/components/layout';
+
+import utilStyles from '@/styles/utils.module.css';
 
 export default function Home() {
   return (
-    <>
+    <Layout home>
       <Head>
         <title>Next review</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Home</h1>
+      <section className={utilStyles.headingMd}>
+        <h1>Welcome to Home</h1>
         <h2>
           <Link href="/creatures">Creatures</Link>
         </h2>
-      </main>
-    </>
+      </section>
+    </Layout>
   )
 }
