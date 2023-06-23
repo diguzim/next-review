0e1f8d97e721e46cef4ed1c5ee7e49589ae82c3b
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-06-23 12:21:40 -0300

    Starting to make use of getStaticProps to get the data for creatures page

--- pages/creatures/index.tsx
+++ pages/creatures/index.tsx
@@ -2,16 +2,47 @@ import Link from "next/link";
 import Head from "next/head";
 import Layout from "@/components/layout";
 
-export default function Creatures() {
-    return (
-        <Layout>
-          <Head>
-            <title>Creatures</title>
-          </Head>
-          <h1>Creatures</h1>
-          <h2>
-            <Link href="/">Back to home</Link>
-          </h2>
-        </Layout>
-      );
+import utilStyles from '@/styles/utils.module.css';
+
+export default function Creatures({ creatures }: { creatures: any[] }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Creatures</title>
+      </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Creatures</h2>
+        <ul className={utilStyles.list}>
+          {creatures.map(({ id, name }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {name}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const creatures = [
+    {
+      "id": "6490481c8f6dd2b0bd3bafbb",
+      "name": "Orc"
+    },
+    {
+      "id": "6490483b39a7933ff1535e09",
+      "name": "Troll"
+    },
+    {
+      "id": "6490486679ba4f9ff19e1609",
+      "name": "Demon"
+    }
+  ]
+
+  return {
+    props: {
+      creatures
+    },
+  };
 }
