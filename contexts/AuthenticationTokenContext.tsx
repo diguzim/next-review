fe3394937bbf8af61bf432e17410e8a5796e1631
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-07-03 23:53:36 -0300

    Simpligying _app.tsx by creating AuthenticationTokenProvider

--- contexts/AuthenticationTokenContext.tsx
+++ contexts/AuthenticationTokenContext.tsx
@@ -1,5 +1,5 @@
 import { AuthenticationToken } from "@/types";
-import { createContext } from "react";
+import { Dispatch, ReactNode, SetStateAction, createContext, useEffect, useState } from "react";
 
 interface AuthenticationTokenContextProps {
   authenticationToken: AuthenticationToken;
@@ -7,3 +7,32 @@ interface AuthenticationTokenContextProps {
 }
 
 export const AuthenticationTokenContext = createContext<AuthenticationTokenContextProps>({} as AuthenticationTokenContextProps);
+
+export const AuthenticationTokenProvider = ({ children }: { children: ReactNode }) => {
+  const [authenticationToken, setAuthenticationToken] = useState<AuthenticationToken>(null);
+
+  useEffect(() => {
+    setAuthenticationToken(localStorage.getItem('authenticationToken'));
+  }, [setAuthenticationToken]);
+
+  function setAuthenticationTokenAndLocalStorage(authenticationToken: AuthenticationToken) {
+    if (authenticationToken === null) {
+      localStorage.removeItem('authenticationToken');
+    } else {
+      localStorage.setItem('authenticationToken', authenticationToken);
+    }
+
+    setAuthenticationToken(authenticationToken);
+  }
+
+  return (
+    <AuthenticationTokenContext.Provider
+      value={{
+        authenticationToken,
+        setAuthenticationToken: setAuthenticationTokenAndLocalStorage as Dispatch<SetStateAction<AuthenticationToken>>
+      }}
+    >
+      {children}
+    </AuthenticationTokenContext.Provider>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,39 +2,14 @@ import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app'
 
 import '@/styles/globals.css'
-import { AuthenticationTokenContext } from '@/contexts';
-import { useEffect, useState } from 'react';
-
-import { Dispatch, SetStateAction } from 'react';
-import { AuthenticationToken } from '@/types';
+import { AuthenticationTokenProvider } from '@/contexts';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [authenticationToken, setAuthenticationToken] = useState<AuthenticationToken>(null);
-
-  useEffect(() => {
-    setAuthenticationToken(localStorage.getItem('authenticationToken'));
-  }, [setAuthenticationToken]);
-
-  function setAuthenticationTokenAndLocalStorage(authenticationToken: AuthenticationToken) {
-    if (authenticationToken === null) {
-      localStorage.removeItem('authenticationToken');
-    } else {
-      localStorage.setItem('authenticationToken', authenticationToken);
-    }
-
-    setAuthenticationToken(authenticationToken);
-  }
-  
   return(
-    <AuthenticationTokenContext.Provider
-      value={{
-        authenticationToken,
-        setAuthenticationToken: setAuthenticationTokenAndLocalStorage as Dispatch<SetStateAction<AuthenticationToken>>
-      }}
-    >
+    <AuthenticationTokenProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout> 
-    </AuthenticationTokenContext.Provider>
+    </AuthenticationTokenProvider>
   );
 }
